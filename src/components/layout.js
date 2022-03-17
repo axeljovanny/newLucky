ced2b73cc55b52bc4211b6fa1253f96ce0aa9f0e
Axel Jovanny Quintero Trujillo <axel.jovanny.aj@gmail.com>
2022-03-17 14:19:46 -0600

    Se pasa setTimeout directamente en el Layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,8 +2,9 @@ import React from "react"
 import PropTypes from "prop-types"
 import { GlobalStyles } from '../styles/js/index';
 import '../styles/css/splash.css'
-import './main'
-import { StaticImage } from "gatsby-plugin-image";
+import { StaticImage } from "gatsby-plugin-image"
+import $ from 'jquery'
+
 
 
 if (typeof window !== "undefined") {
@@ -11,49 +12,56 @@ if (typeof window !== "undefined") {
   require("smooth-scroll")('a[href*="#"]')
 }
 
-const Layout = ({ children }) => (
+const Layout = ({ children }) => {
 
-  <>
-    <meta
-      name='viewport'
-      content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'
-    />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-    <GlobalStyles />
-    <div id="loader-wrapper">
-      <div id="loader">
-        <div class="imagen">
-          <StaticImage src="../images/logoSplash.png" width={450} height={450} alt='logo splash' formats={['auto', 'webp']}></StaticImage>
-        </div>
-        <svg width="0" height="0">
-          <filter id="gooey-fill">
-            <feGaussianBlur in="SourceGraphic" stdDeviation="20" result="blur" />
-            <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 50 -16" result="goo" />
-          </filter>
-        </svg>
-        <div class="fill">
-          <div class="gooey-container">
-            <span class="level">
-              <span class="bubble"></span>
-              <span class="bubble"></span>
-              <span class="bubble"></span>
-              <span class="bubble"></span>
-              <span class="bubble"></span>
-              <span class="bubble"></span>
-              <span class="bubble"></span>
-              <span class="bubble"></span>
-            </span>
+  setTimeout(function () {
+    $('body').addClass('loaded');
+    $('h1').css('color', '#222222');
+  }, 4000);
+
+  return (
+    <>
+      <meta
+        name='viewport'
+        content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'
+      />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      <GlobalStyles />
+      <div id="loader-wrapper">
+        <div id="loader">
+          <div class="imagen">
+            <StaticImage src="../images/logoSplash.png" width={450} height={450} alt='logo splash' formats={['auto', 'webp']}></StaticImage>
           </div>
+          <svg width="0" height="0">
+            <filter id="gooey-fill">
+              <feGaussianBlur in="SourceGraphic" stdDeviation="20" result="blur" />
+              <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 50 -16" result="goo" />
+            </filter>
+          </svg>
+          <div class="fill">
+            <div class="gooey-container">
+              <span class="level">
+                <span class="bubble"></span>
+                <span class="bubble"></span>
+                <span class="bubble"></span>
+                <span class="bubble"></span>
+                <span class="bubble"></span>
+                <span class="bubble"></span>
+                <span class="bubble"></span>
+                <span class="bubble"></span>
+              </span>
+            </div>
+          </div>
+
+
         </div>
-
-
+        <div class="loader-section section-right"></div>
       </div>
-      <div class="loader-section section-right"></div>
-    </div>
-    <main>{children}</main>
-  </>
+      <main>{children}</main>
+    </>
+  )
 
-)
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
