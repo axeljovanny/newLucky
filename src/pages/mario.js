d69990e4252dbbfd1156fd943198bfa46dca77d5
Mario <lmariogc41@gmail.com>
2023-05-17 15:57:26 -0600

    se crean secciones

--- src/pages/mario.js
+++ src/pages/mario.js
@@ -1,15 +1,50 @@
 import React from "react" // Libreria de React
-import { } from "../styles/js/mario/mario";
+import { SAbout, SContenedorPrincipal, SFooter, SHeader, SHeader1, SHeader2, SHeader3, SPortafolio, SServicios, STeam } from "../styles/js/mario/mario";
 
 
 //Pagina principal Mario
 const MarioPage = () => {
-    return (
+  return (
     //Aqui se agrega todo lo que va en tu pagina
-      <>
-      
-      </>
-    )
-  };
+    <>
+      <SContenedorPrincipal>
+        <SHeader>
+          <SHeader1>
+            <SHeader2>
 
-  export default MarioPage
+            </SHeader2>
+
+      <SHeader3></SHeader3>
+      
+          </SHeader1>
+        </SHeader>
+
+
+        <SAbout>
+        <h1> ABOUT</h1>
+        </SAbout>
+
+
+        <SPortafolio>
+        <h1> PORTAFOLIO </h1>
+        </SPortafolio>
+
+        <SServicios>
+        <h1> SERVICIO </h1>
+       </SServicios>
+
+       <STeam>
+        <h1> TEAM  </h1>
+       </STeam>
+
+      <SFooter>
+        <h1> FOOTER </h1>
+      </SFooter>
+
+      </SContenedorPrincipal>
+    </>
+  )
+};
+
+export default MarioPage
+
--- src/styles/js/mario/mario.js
+++ src/styles/js/mario/mario.js
@@ -8,7 +8,113 @@ Nomenclatura de Estilos:
 */
 
 
-export const ContenedorPrincipal = styled.div`
+export const SContenedorPrincipal = styled.div`
+ /* Comentario en estilos */
+width: 100vw;    /* ancho de mi pagina    */ 
+height: auto;    /* largo de mi pagina */
+background:  black; /* color de fondo*/
+`
+
+
+export const SHeader = styled.div`
  /* Comentario en estilos */
 
+width: 100%;    // ancho de mi pagina  
+height: 100vh;    // largo de mi pagina
+background: red;  /* color de fondo*/
+display: flex;  // utilizar las funciones de flex
+justify-content: center; // centrar contenido
+align-items: center; // alinear el contenido
+
 `
+export const SHeader1 = styled.div`
+ /* Comentario en estilos */
+
+width: 98%;    // ancho de mi pagina  
+height: 98%;    // largo de mi pagina
+background: white;  /* color de fondo*/
+display: flex;  // utilizar las funciones de flex
+flex-direction: row;
+`
+export const SHeader2 = styled.div`
+ /* Comentario en estilos */
+
+width: 100%;    // ancho de mi pagina  
+height: 30%;    // largo de mi pagina
+background: blue;  /* color de fondo*/
+display: flex;  // utilizar las funciones de flex
+justify-content: center;
+`
+export const SHeader3 = styled.div`
+ /* Comentario en estilos */
+
+ width: 100%;    // ancho de mi pagina  
+height: 70%;    // largo de mi pagina
+background: green;  /* color de fondo*/
+display: none;  // utilizar las funciones de flex: ;
+
+
+`
+
+export const SAbout = styled.div`
+ /* Comentario en estilos */
+
+width: 100%;    // ancho de mi pagina    
+height: 50vh;    // largo de mi pagina
+background: blue; //color de fondo
+display: flex;  // utilizar las funciones de flex
+justify-content: center;
+align-items: center;
+
+`
+
+
+export const SPortafolio = styled.div`
+ /* Comentario en estilos */
+
+width: 100%;    // ancho de mi pagina    
+height: 100vh;    // largo de mi pagina
+background: yellow; //color de fondo
+display: flex;  // utilizar las funciones de flex
+justify-content: center;
+align-items: center;
+
+`
+
+
+export const SServicios = styled.div`
+ /* Comentario en estilos */
+
+width: 100%;    // ancho de mi pagina    
+height: 100vh;    // largo de mi pagina
+background: green; //color de fondo
+display: flex;  // utilizar las funciones de flex
+justify-content: center;
+align-items: center;
+
+`
+
+export const STeam = styled.div`
+ /* Comentario en estilos */
+
+width: 100%;    // ancho de mi pagina    
+height: 100vh;    // largo de mi pagina
+background: purple; //color de fondo
+display: flex;  // utilizar las funciones de flex
+justify-content: center;
+align-items: center;
+
+`
+
+export const SFooter = styled.div`
+ /* Comentario en estilos */
+
+width: 100%;    // ancho de mi pagina    
+height: 60vh;    // largo de mi pagina
+background: maroon; //color de fondo
+display: flex;  // utilizar las funciones de flex
+justify-content: center;
+align-items: center;
+
+`
+
